5d5c30814ed33ac778caf42dc3b01248c561ab76
“VijayMeena701” <govardhanmeena701@gmail.com>
2021-03-01 12:42:05 +0530

    Optimized and updated Homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,61 @@
 import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
 import bookMain from './bookMain.png';
 
 
 const styles = (theme)=> ({
-    ImgContainer: {
-        maxWidth: '1920px',
-        maxHeight: '1288px',
-        backgroundImage: `linear-gradient(rgba(51,51,51,0.5), rgba(51,51,51,0.5)), url(${bookMain})`,
-        backgroundRepeat: 'no-repeat',
-        backgroundSize: '100%',
+    root: {
+        background: '#222',
     },
-    containerLayer: {
-        height: '100vh',
+    ImgContainer: {
+        backgroundImage: `url(${bookMain})`,
+        backgroundSize: '100%',
+        backgroundRepeat: 'no-repeat',
+    },
+    textContainer: {
+        postion: 'relative',
+        background: 'rgba(51,51,51, 0.5)',
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        height: '66.083vw',
+        verticalAlign: 'middle',
+        textAlign: 'center',
+        '& div': {
+            width: 'auto',
+            height: 'auto',
+            margin: 'auto',
+            position: 'relative',
+            paddingBottom: '18%',
+            '& p': {
+                // fontFamily: 'Lato',
+                fontSize: '60px',
+                fontStyle: 'normal',
+                fontWeight: '900',
+                lineHeight: '80px',
+                letterSpacing: '0em',
+                textAlign: 'center',
+                color: '#fff',
+            },
+            '& Button': {
+                background: '#189ff9',
+                color: '#fff',
+                borderRadius: '27.5px',
+                fontSize: '20px',
+                padding: '1% 5%',
+                fontWeight: 400,
+                marginTop: '30px'
+            }
+        },
+    },
+    secondSec: {
+        background: '#fff',
+        marginTop: '-30vh',
+        borderRadius: '0 250px 0 0',
+        height: '200vh',
     }
 })
 
@@ -21,11 +63,22 @@ function Home(props) {
     const classes = props.classes;
     return (
         <>
-            <div className={classes.ImgContainer}>
-                <div className={classes.containerLayer}>
+            <section className={classes.root}>
+                <div className={classes.ImgContainer}>
+                    <div className={classes.textContainer}>
+                        <div>
+                            <p>Together we're building</p>
+                            <p>brighter futures</p>
+                            <Link to="/about">
+                            <Button>Learn More</Button>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.secondSec}>
 
                 </div>
-            </div>
+            </section>
         </>
     )
 }
