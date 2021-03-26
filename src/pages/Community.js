58f714475ec478e4e991d3424724f0f048d13597
“VijayMeena701” <govardhanmeena701@gmail.com>
2021-03-26 21:01:48 +0530

    Fixed useInstFeed Component

--- src/pages/Community.js
+++ src/pages/Community.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { useInstagramFeed } from "../hooks/useInstagramFeed";
+// import { useInstagramFeed } from "../hooks/useInstagramFeed";
 import withStyles from "@material-ui/core/styles/withStyles";
 
 const styles = (theme) => ({
